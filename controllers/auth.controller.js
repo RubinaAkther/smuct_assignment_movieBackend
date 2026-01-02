dbec266107c032d513a6b8975f81f03eb7aa584e
Rubina Akther <rubinaakther3454@gmail.com>
2026-01-02 13:01:18 +0600

    Store registered user data

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,3 +1,5 @@
+const User = require('../models/user.model.js');
+
 const home = async (req, res) => {
   try {
     res.status(200).send('This is a home page from controller file');
@@ -8,11 +10,20 @@ const home = async (req, res) => {
 
 const register = async (req, res) => {
   try {
-    console.log(req.body)
-    res.status(200).send({message: req.body});
+    console.log(req.body);
+    const { username, email, phone, password } = req.body;
+
+    const userExist = await User.findOne({ email });
+    if (userExist) {
+      return res.status(400).json({ msg: 'user already exits' });
+    }
+
+    const userCreated = await User.create({ username, email, phone, password });
+
+    res.status(200).send({ userCreated });
   } catch (error) {
     res.status(404).send({ message: 'page not found' });
   }
 };
 
-module.exports = { home ,register};
+module.exports = { home, register };
